9d5fb98041f8972484cf1c1e7638092000fa04ff
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-02 18:13:11 +0530

    Created Title Component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,14 @@
 import { StyleSheet, Text,View } from "react-native";
 
+import Title from "../components/Title";
+
 function GameScreen(){
     return (
-        <View>
-            <Text>Guess number</Text>
+        <View style={styles.screen}>
+            <Title style={styles.title}>Opponent's Guess</Title>
+            <View>
+                <Text>Higher or lower?</Text>
+            </View>
         </View>
     )
 }
@@ -13,5 +18,15 @@ export default GameScreen;
 const styles=StyleSheet.create({
     screen:{
         flex:1,
+        padding:24
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+        color:'#ddb52f',
+        textAlign:'center',
+        borderWidth:2,
+        borderColor:'#ddb52f',
+        padding:12
     }
 })
